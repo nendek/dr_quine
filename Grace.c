77dd6dab34443a172c97cb290b45fb71fc407580
Pierre NARDOZI <pnardozi@e1r3p3.42.fr>
2019-09-02 12:36:43 +0200

    fix Grace.c

--- Grace.c
+++ Grace.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 /*
 comment
 */
-#define STRING "#include <stdio.h>%2$c#include <unistd.h>%2$c%4$c%3$c%2$ccomment%2$c%3$c%4$c%2$c#define STRING %5$c%1$s%5$c%2$c#define PRINT(fd) fprintf(fd,STRING,STRING,10,42,47,34)%2$c#define FUNC() int main(){FILE *fd;fd=fopen(%5$cGrace_kid.c%5$c, %5$cw%5$c);if(fd>0){PRINT(fd);}return(0);}%2$cFUNC()%2$c"
-#define PRINT(fd) fprintf(fd,STRING,STRING,10,42,47,34)
-#define FUNC() int main(){FILE *fd;fd=fopen("Grace_kid.c", "w");if(fd>0){PRINT(fd);}return(0);}
+#define STRING "#include <stdio.h>%2$c#include <unistd.h>%2$c#include <fcntl.h>%2$c%4$c%3$c%2$ccomment%2$c%3$c%4$c%2$c#define STRING %5$c%1$s%5$c%2$c#define PRINT(fd) dprintf(fd,STRING,STRING,10,42,47,34)%2$c#define FUNC() int main(){int fd;fd=open(%5$cGrace_kid.c%5$c, O_CREAT | O_WRONLY | O_TRUNC);if(fd!=-1){PRINT(fd);}return(0);}%2$cFUNC()%2$c"
+#define PRINT(fd) dprintf(fd,STRING,STRING,10,42,47,34)
+#define FUNC() int main(){int fd;fd=open("Grace_kid.c", O_CREAT | O_WRONLY | O_TRUNC);if(fd!=-1){PRINT(fd);}return(0);}
 FUNC()
